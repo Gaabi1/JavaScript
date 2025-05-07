c473a8a66c2f8477ad45ad8284cdef10da7aec2a
Gabi <gabrielaa.ester@gmail.com>
2025-05-07 11:09:42 -0300

    novo commit

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais= 'eua'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil' || pais == 'brasil'){
+    console.log('Voce é brasileiro')
+} else {
+    console.log('Voce é estrangeiro')
+}

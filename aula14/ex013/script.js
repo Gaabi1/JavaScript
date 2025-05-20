98437802b0fe135d897828298bf3b9ba3abcdde6
Gabi <gabrielaa.ester@gmail.com>
2025-05-20 11:17:40 -0300

    novo commit

--- aula14/ex013/script.js
+++ aula14/ex013/script.js
@@ -1,11 +1,21 @@
 function contar() {
-    var beg = document.getElementById('txtb')
-    var fim = document.getElementById('txte')
-    var passo = document.getElementById('txtp')
+    let beg = document.getElementById('txtb')
+    let fim = document.getElementById('txte')
+    let passo = document.getElementById('txtp')
+    let resp = document.getElementById('resp')
+
 
     if (beg.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
-        window.alert('[ERRO]verifique os dados e tente novamente')
+        resp.innerHTML = 'Impossivel contar!'
+        //window.alert('[ERRO]verifique os dados e tente novamente')
     }else{
-       window.alert('Tudo ok')
+        resp.innerHTML = 'Contando: '
+        let b = Number(beg.value)
+        let e = Number(fim.value)
+        let p = Number(passo.value)
+
+        for( let i = b; i<= e; i+= p){
+            resp.innerHTML += `${i} `
+        }
     }
 }
